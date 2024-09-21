c99a27a0e94df91a05c49d9175da98ee9725514f
Jerry Sandhu <jsa2282@wgu.edu>
2024-09-21 16:17:10 -0700

    Add assistant models and initial messages by bot

--- main.py
+++ main.py
@@ -10,11 +10,35 @@ load_dotenv()
 
 
 async def entrypoint(ctx: JobContext):
-    intial_ctx = llm.ChatContext().append(
+    initial_ctx = llm.ChatContext().append(
         role="system",
-        text=("Hello, I am a voice assistant. How can I help you today?"),
+        text=("You will act as an AI voice assistant that responds and interfaces with usesr onyl by voice,"
+              "Give short and concise responses to user queries."),
     )
+
+    # Subscribe to audio track, no video
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_ctx = AssistantFnc()
+
+    assistant = VoiceAssistant(
+        # Voice activity detection
+        vad=silero.VAD.load(),
+        # Speech to text
+        stt=openai.STT(),
+        # Language model
+        llm=openai.LLM(),
+        # Text to speech
+        tts=openai.TTS(),
+        chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
+    )
+    # Start audio only room from Livekit
+    assistant.start(ctx.room)
+
+    await asyncio.sleep(1)
+    await assistant.say("Hello, I am a voice assistant. How can I help you today?", allow_interruptions=True)
 
 
 if __name__ == "__main__":
+    # Starts chat agent
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
